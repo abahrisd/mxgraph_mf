d72af2092d61484d855350971f2dac7d0c93f3d0
SAbahri <SAbahri@ibs.ru>
2016-06-02 19:46:34 +0500

    remove clog from mxClient

--- javascript/examples/grapheditor/src/js/mxClient.js
+++ javascript/examples/grapheditor/src/js/mxClient.js
@@ -57766,11 +57766,6 @@ mxGraph.prototype.cellSizeUpdated = function(cell, ignoreChildren)
 						var tr = this.view.translate;
 						var scale = this.view.scale;
 
-                        console.log("bounds",bounds);
-                        console.log("scale",scale);
-                        console.log("geo",geo);
-                        console.log("tr",tr);
-
 						var width = (bounds.x + bounds.width) / scale - geo.x - tr.x;
 						var height = (bounds.y + bounds.height) / scale - geo.y - tr.y;
 
